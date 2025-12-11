41521e4d2100c447d8b2e57b8c3c4af43ad73d15
theRebelliousNerd <steve@nextgenrd.com>
2025-12-11 14:43:06 -0500

    push

---
+++ repro_gap.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+// UnusedFunction is a public function that is never called.
+// This should be detected as a wiring gap.
+func UnusedFunction() {
+	fmt.Println("I am lonely.")
+}
